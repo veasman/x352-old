e2d6be41f7fe4ce8af930a6d8de126faef8c360d
veasman <charlton.moren@gmail.com>
2021-07-07 16:18:29 -0500

    Fix prediction interface

--- src/core/features/movement.cpp
+++ src/core/features/movement.cpp
@@ -17,40 +17,13 @@ void Features::Movement::storeFlags(CUserCmd* cmd) {
 // TODO: Do proper edge detection, and speed adjustment
 void Features::Movement::edgeBug(CUserCmd* cmd) {
     if (CONFIGBOOL("Misc>Misc>Movement>Edge Bug") &&
-        Menu::CustomWidgets::isKeyDown(CONFIGINT("Misc>Misc>Movement>Edge Bug Key")) &&
+        Menu::CustomWidgets::isKeyDown(CONFIGINT("Misc>Misc>Movement>Edge Bug Key"))) { /*&&
         Globals::localPlayer->moveType() != MOVETYPE_LADDER &&
         Globals::localPlayer->moveType() != MOVETYPE_NOCLIP &&
         !(storedFlags & FL_ONGROUND) && // Predicting we are going to hit the ground
         Globals::localPlayer->flags() & FL_ONGROUND) {
-        cmd->buttons |= IN_DUCK;
-        /*int stage = 0;
-        float oldVel = 0.f;
-        float cooldown = 0.f;
-        int numEB = 0;
-        int numConsecEB = 0;
-        Vector vel = Globals::localPlayer->velocity();
-        AnimState* anim = Globals::localPlayer->animState();
+        cmd->buttons |= IN_DUCK;*/
 
-        if (!anim->onGround) {
-            stage = 1;
-            if (vel.z < 1.f && oldVel < vel.z) {
-                stage = 2;
-                if (anim->horizontalVelocity > 10.f) {
-                    stage = 3;
-                    if (Interfaces::globals->curtime > cooldown) {
-                        stage = 4;
-                        cooldown = Interfaces::globals->curtime + 0.5f;
-                        numEB += 1;
-                        Globals::didEb = true;
-                    }
-                }
-            }
-        } else {
-            // Reset counter for consecutive EB
-            numConsecEB = 0;
-        }
-
-        oldVel = vel.z;*/
     }
 }
 
--- src/sdk/interfaces/iprediction.hpp
+++ src/sdk/interfaces/iprediction.hpp
@@ -2,7 +2,66 @@
 #include "iclientmode.hpp"
 
 class Entity;
-class CMoveData;
+
+class CMoveHelper {
+public:
+	bool	first_run_of_iunctions : 1;
+	bool	game_code_moved_player : 1;
+	int	    player_handle;
+	int	    impulse_command;
+	Vector	view_angles;
+	Vector	abs_view_angles;
+	int	    buttons;
+	int	    old_buttons;
+	float	forward_move;
+	float	side_move;
+	float	up_move;
+	float	max_speed;
+	float	client_max_speed;
+	Vector	velocity;
+	Vector	angles;
+	Vector	old_angles;
+	float	out_step_height;
+	Vector	wish_velocity;
+	Vector	jump_velocity;
+	Vector	constraint_center;
+	float	constraint_radius;
+	float	constraint_width;
+	float	constraint_speed_factor;
+	float	u0[5];
+	Vector	abs_origin;
+	virtual	void u1() = 0;
+	virtual void setHost(Entity* host) = 0;
+};
+
+class CMoveData {
+public:
+	bool    first_run_of_instructions : 1;
+	bool    game_code_moved_player : 1;
+	int     player_handle;
+	int     impulse_command;
+	Vector	view_angles;
+	Vector	abs_view_angles;
+	int     buttons;
+	int     old_buttons;
+	float   fw_move;
+	float   sd_move;
+	float   up_move;
+	float   max_speed;
+	float   client_max_speed;
+	Vector	velocity;
+	Vector	angles;
+	Vector	old_angles;
+	float   step_height;
+	Vector	wish_velocity;
+	Vector	jump_velocity;
+	Vector	constraint_center;
+	float   constraint_radius;
+	float   constraint_width;
+	float   constraint_speed_factor;
+	float   u0[ 5 ];
+	Vector	abs_origin;
+};
 
 class IGameMovement {
 public:
@@ -22,18 +81,20 @@ public:
 	}
 };
 
-class IMoveHelper {
-public:
-	void SetHost(Entity* host) {
-		typedef void (*Fn)(void* , Entity* );
-		return getVirtualFunc<Fn>(this, 1)(this, host);
-	}
-};
-
 class IPrediction {
 public:
-	void SetupMove(Entity* player, CUserCmd* cmd, IMoveHelper* helper, CMoveData* move) {
-		typedef void (*Fn)(void* , Entity* , CUserCmd* , IMoveHelper* , CMoveData* );
+    bool InPrediction() {
+        typedef bool (*Fn)(void*);
+        return getVirtualFunc<Fn>(this, 14)(this);
+	}
+
+	void RunCommand(Entity* player, CUserCmd* cmd, CMoveHelper* helper ) {
+        typedef void (*Fn)(void*, Entity*, CUserCmd*, CMoveHelper*);
+        return getVirtualFunc<Fn>(this, 19)(this, player, cmd, helper);
+	}
+
+	void SetupMove(Entity* player, CUserCmd* cmd, CMoveHelper* helper, CMoveData* move) {
+		typedef void (*Fn)(void* , Entity* , CUserCmd* , CMoveHelper* , CMoveData* );
 		return getVirtualFunc<Fn>(this, 21)(this, player, cmd, helper, move);
 	}
 
